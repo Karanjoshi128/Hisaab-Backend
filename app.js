dc92c22cc2e23a85450eefeaa6a9e858a2f15302
Karan Joshi <joshikaran.aad.0007@gmail.com>
2024-07-19 23:41:39 +0530

    chore: Update CORS origin for production environment

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-    // origin: 'http://localhost:5173', // Development
-    origin: 'https://hisaab-frontend.vercel.app/', // Production
+    origin: 'https://hisaab-frontend.vercel.app', // Production
     credentials: true // Allow credentials to be sent
   }));
 app.use(express.json());
