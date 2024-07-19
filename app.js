bd9416cb0592f19311aaa68612e0277595cf5b83
Karan Joshi <joshikaran.aad.0007@gmail.com>
2024-07-20 00:04:42 +0530

    Refactor import statement in dbConnect.js to use correct DB_NAME constant

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use(cookieParser());
 app.use(cors({
     origin: 'https://hisaab-frontend.vercel.app', // Production
     credentials: true // Allow credentials to be sent
-  }));
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 import userRouter from "./src/routes/user.routes.js";
 app.use("/api/users/v1" , userRouter); 
